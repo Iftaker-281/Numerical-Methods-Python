f48c9e18a01c9d2f089bcc1f9270f21ced504d31
Iftaker Ahmed Tahmid <ahmedeftakhertahmid@gmail.com>
2026-09-22 12:41:22 +0600

    Create fixed_point.py

---
+++ 02_Root_Finding/fixed_point.py
@@ -0,0 +1,16 @@
+def f(x):
+    return x**3+x-1
+def g(x):
+    return (1-x)**(1/3)
+
+x0 = 0.5
+Es = 0.005
+x_old = x0
+for i in range(20):
+    x_new = g(x_old)
+    error = abs(x_new - x_old)
+    print("Iteration = ",i+1,"Error = ",error,"New x is: ",round(x_new,4))
+    if error<Es:
+        break
+    x_old = x_new
+print("Root = ",round(x_new,4))
