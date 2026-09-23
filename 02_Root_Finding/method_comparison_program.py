9bac0d99eb0df2f35175e694469a88e25622b5a1
Iftaker Ahmed Tahmid <ahmedeftakhertahmid@gmail.com>
2026-09-23 16:33:04 +0600

    Update method_comparison_program.py

--- 02_Root_Finding/method_comparison_program.py
+++ 02_Root_Finding/method_comparison_program.py
@@ -1,34 +1,77 @@
 def f(x):
     return x**3 - x - 2
-def df(x):
-    return 3*x**2-1
-    
-def bisection(a,b):
-    for i in range(20):
-        mid = (a+b)/2
-        if abs(f(mid))<0.005:
-            return i+1,mid
-        if f(a) * f(mid)<0:
-            b = mid
-        else:
-            a = mid
-    return i+1,mid
-def newton_rapshon(x):
-    for i in range(20):
-        x_new = x - f(x)/df(x)
-        if abs(x_new - x)<0.005:
-            return i+1,x_new
-        x = x_new
-    return i+1,x_new
 
-def secant(x0,x1):
+def df(x):
+    return 3*x**2 - 1
+
+
+def bisection(a,b,tolerance):
+
+    x_old = (a+b)/2
+
     for i in range(20):
-        x_new = x1 - f(x1) * (x1 - x0)/(f(x1)-f(x0))
-        if abs(x_new - x1)<0.005:
-            return i+1,x_new
+
+        x_new = (a+b)/2
+
+        error = abs(x_new-x_old)/abs(x_new)
+
+        if f(a)*f(x_new) < 0:
+            b = x_new
+        else:
+            a = x_new
+
+        print("Bisection Iteration",i+1,"Error",error,"Root",x_new)
+
+        if error < tolerance:
+            return i+1,x_new,error
+
+        x_old = x_new
+
+
+def newton(x_old,tolerance):
+
+    for i in range(20):
+
+        x_new = x_old - f(x_old)/df(x_old)
+
+        error = abs(x_new-x_old)/abs(x_new)
+
+        print("Newton Iteration",i+1,"Error",error,"Root",x_new)
+
+        if error < tolerance:
+            return i+1,x_new,error
+
+        x_old = x_new
+
+
+def secant(x0,x1,tolerance):
+
+    for i in range(20):
+
+        x_new = x1 - f(x1)*(x1-x0)/(f(x1)-f(x0))
+
+        error = abs(x_new-x1)/abs(x_new)
+
+        print("Secant Iteration",i+1,"Error",error,"Root",x_new)
+
+        if error < tolerance:
+            return i+1,x_new,error
+
         x0 = x1
         x1 = x_new
-    return i+1,x_new
-print("Bisection = ",bisection(1,2))
-print("Newton = ",newton_rapshon(1))
-print("Secant = ",secant(1,2))
+
+
+tolerance = 0.0001
+
+iteration1,root1,error1 = bisection(1,2,tolerance)
+
+iteration2,root2,error2 = newton(1.5,tolerance)
+
+iteration3,root3,error3 = secant(1,2,tolerance)
+
+
+print()
+print("Approach       Iteration       Final Root")
+print("Bisection      ",iteration1,"          ",root1)
+print("Newton         ",iteration2,"          ",root2)
+print("Secant         ",iteration3,"          ",root3)
